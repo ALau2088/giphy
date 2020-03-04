bd317d0327204310e953d7e14317fb0c2d267728
ALau2088 <alau12004@yahoo.com>
2020-03-03 16:07:12 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: '',
+      url: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ keyword: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${this.state.keyword}&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1`
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ url: res.data.data[0]['embed_url'] });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Rhumbix Programming Test</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            value={this.state.keyword}
+            onChange={this.handleChange}
+          />
+          <input type='submit' value='Go' />
+        </form>
+        <div>{this.state.url}</div>
+        <img src={this.state.url} alt='...loading' height='42' width='42' />
+      </div>
+    );
+  }
+}
+
+export default App;
